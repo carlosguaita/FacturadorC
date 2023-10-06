1daca24b3533052d4596efde9ecc1a86b606b809
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-10-06 15:11:37 -0500

    se termina la logica de condicionales

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(void) {
   float numa=0, numb=0, numc=0, numd=0, nume=0;
   float precioa=150, preciob=55, precioc=180, preciod=70, precioe=120;
   char select1;
-  float subta=0, subtb=0, subtc=0, subtd=0, subte=0, subtotal=0;
-
+  float subta=0, subtb=0, subtc=0, subtd=0, subte=0, subtotal=0, subtotaldes=0, total=0;
+  char nombre[50], cedula[20];
 
   printf("---------------BIENVENIDOS-------------------------\n");
   printf("Elija que producto desea facturar:\n");
@@ -21,7 +21,6 @@ int main(void) {
       printf("Ingrese la cantidad de Llantas que desea comprar: ");
       scanf("%f",&numa);
       subta=precioa*numa;
-      
     break;
     case 'b':
       printf("Ingrese la cantidad de Pastillas de freno que desea comprar: ");
@@ -45,8 +44,36 @@ int main(void) {
     break;
   }
   subtotal=subta+subtb+subtc+subtd+subte;
-  
-  
-  printf("Hello World\n");
+
+if (subtotal>100 && subtotal<=500){
+  subtotaldes=subtotal*0.95;
+}else if(subtotal>500 && subtotal<=1000){
+  subtotaldes=subtotal*0.93;
+}else if(subtotal>1000){
+  subtotaldes=subtotal*0.90;
+}
+
+total=subtotaldes*1.12;
+
+printf("Ingrese el nombre del cliente\n");
+scanf("%s",&nombre);
+
+printf("Ingrese el numero de cedula\n");
+scanf("%s",&cedula);
+
+printf("---------------Factura--------------\n");
+printf("Nombre del cliente: %s\n",nombre);
+printf("Cedula del cliente: %s\n",cedula);
+printf("Producto\t\tCantidad\t\tSubtotal\n");
+printf("Llantas\t\t%f\t\t%f\n",numa,subta);
+printf("Pastillas Freno\t\t%f\t\t%f\n",numb,subtb);
+printf("Embrague\t\t%f\t\t%f\n",numc,subtc);
+printf("Faro\t\t%f\t\t%f\n",numd,subtd);
+printf("Radiador\t\t%f\t\t%f\n",nume,subte);
+printf("Sub total\t%f\n",subtotal);
+printf("Sub total con descuento\t%f\n",subtotaldes); 
+  printf("Total\t%f\n",total);
+
+
   return 0;
 }
